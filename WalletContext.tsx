bbb925c17c0085be1ff3650d3041b1232279ef44
ThaRealCryp <162860738+ThaRealCryp@users.noreply.github.com>
2026-05-09 01:37:50 -0500

    Remove unused import from WalletContext.tsx
    Removed unused import for clusterApiUrl.

--- WalletContext.tsx
+++ WalletContext.tsx
@@ -1,7 +1,6 @@
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
 import { PhantomWalletAdapter } from '@solana/wallet-adapter-phantom';
-import { clusterApiUrl } from '@solana/web3.js';
 import { FC, ReactNode, useMemo } from 'react';
 
 interface WalletContextProviderProps {
